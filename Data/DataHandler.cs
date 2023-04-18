edf4946c47d02b4a2c031e4b1559980bd5dbbfdc
Stephen Ritchie <steve10810@gmail.com>
2023-04-18 14:47:28 +0200

    Add base project

---
+++ Data/DataHandler.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test.Data
+{
+    public static class DataHandler
+    {
+        // Method to get values from input
+        public static string[] GetValuesFromInput()
+        {
+            while (true)
+            {
+                var input = Console.ReadLine();
+                if (input == null) { continue; } // Continue if input is null
+                var values = input
+                    .ToString()
+                    .Split(',', StringSplitOptions.RemoveEmptyEntries); // Split input by comma and remove empty entries
+                return values;
+            }
+        }
+
+        // Method to multiply numbers and uppercase strings
+        public static string MultiplyNumbersAndUpperCaseStrings(string[] data, int multiplier = 1)
+        {
+            var result = new List<string>();
+            foreach (string s in data)
+            {
+                int value;
+                if (int.TryParse(s, out value))
+                {
+                    // If string can be parsed as integer, multiply it by the multiplier
+                    result.Add((value * multiplier).ToString());
+                }
+                else
+                {
+                    // If not, convert string to uppercase
+                    result.Add(s.ToUpper());
+                }
+            }
+            return string.Concat(result); // Concatenate the resulting strings
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Numerics;
+using System.Reflection.Metadata;
+using Test.Data;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var programRunning = true;
+        while (programRunning)
+        {
+            Console.WriteLine("Please input comma separated data");
+            string[] inputData = DataHandler.GetValuesFromInput(); // Get input data as an array of strings
+
+            Console.WriteLine("Please choose a way to handle this data, please enter number corresponding to selected option e.g 1");
+            Console.WriteLine("1. Merge into single string");
+            Console.WriteLine("2. Multiply numbers and uppercase strings");
+            int choice = Convert.ToInt32(Console.ReadLine()); // Get user's choice as an integer
+
+            switch (choice)
+            {
+                case 1:
+                    Console.WriteLine($"Input as single string: {string.Concat(inputData)}"); // Concatenate input data into a single string and display
+                    break;
+                case 2:
+                    Console.WriteLine("Enter value to multiply numbers by:");
+                    int multiplicationFactor = Convert.ToInt32(Console.ReadLine()); // Get multiplication factor from user
+                    Console.WriteLine($"Result: {DataHandler.MultiplyNumbersAndUpperCaseStrings(inputData, multiplicationFactor)}"); // Call a method to multiply numbers and uppercase strings, and display the result
+                    break;
+                default:
+                    Console.WriteLine("Invalid value, please try again"); // Display error message for invalid input
+                    break;
+            }  
+        }
+    }
+}
